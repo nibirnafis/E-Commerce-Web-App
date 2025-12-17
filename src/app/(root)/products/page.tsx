74646e5d7f4cc11139f8cd800401af305e5896b6
nibirnafis <nibirnafis@gmail.com>
2025-12-17 15:35:48 +0600

    check

--- src/app/(root)/products/page.tsx
+++ src/app/(root)/products/page.tsx
@@ -1,7 +1,7 @@
 import Product from '@/components/Product';
 import Title from '@/components/Title';
 import { TProduct } from '@/types/Types';
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
 
 
 
@@ -32,9 +32,9 @@ const page = async() => {
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 md:gap-8 mx-auto max-w-[1440px] px-8 pb-24">
             {
                 products.map((product: TProduct, key: number) => (
-                    // <Suspense fallback={<p>Loading..</p>} key={key}>
+                    <Suspense fallback={<p>Loading..</p>} key={key}>
                         <Product product={product} key={key}></Product>
-                    // </Suspense>
+                    </Suspense>
                 ))
             }
         </div>
